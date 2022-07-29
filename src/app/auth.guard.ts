119f6cef9f562a242621b4c200bcb91a0e8fc35d
diokou_tech <diokoucheikh@gmail.com>
2022-07-29 01:12:02 +0000

    update

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,15 @@ export class AuthGuard implements CanActivate {
       return true;
     }else{
       this.router.navigate([''])
+      Swal.fire({
+        text: "Essayer de vous connecter !",
+        icon: "error",
+        position: 'top-end',
+        toast: true,
+        showConfirmButton: false,
+        timer: 5000,
+        timerProgressBar: true
+      });
       return false;
     }
   }
